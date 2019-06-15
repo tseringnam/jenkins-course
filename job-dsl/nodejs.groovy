d1a3cc1f2e008643cf20f6856a367e4ecdf8d3fc
tseringnam <35940410+tseringnam@users.noreply.github.com>
2019-06-15 16:27:52 -0400

    Update nodejs.groovy

--- job-dsl/nodejs.groovy
+++ job-dsl/nodejs.groovy
@@ -1,6 +1,6 @@
 job('NodeJS example') {
     scm {
-        git('git://github.com/wardviaene/docker-demo.git') {  node -> // is hudson.plugins.git.GitSCM
+        git('git://https://github.com/tseringnam/jenkins-course') {  node -> // is hudson.plugins.git.GitSCM
             node / gitConfigName('DSL User')
             node / gitConfigEmail('jenkins-dsl@newtech.academy')
         }
